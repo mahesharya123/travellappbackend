85e08e59cc7094f066a11fc1bb89a25a7a6fb7cf
Mahesh <mahesharya381@gmail.com>
2023-06-09 16:54:01 +0530

    third commit to add cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors');
 dotenv.config();
 
 
@@ -19,6 +20,7 @@ const categorydataAddtoDBrouter = require("./routes/categoryimport");
 const PORT = 3500;
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 connectDB();
